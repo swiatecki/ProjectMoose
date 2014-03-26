b06d6a374cf49acbde3c0771dc361372f4bacf94
Nicholas Swiatecki <nicholas@swiatecki.com>
2014-03-27 00:08:47 +0100

    Modified Matlab outputs
    in TDN\matlab.m and model01.m

--- Code/Tests/trackDelayNetwork/matlab.m
+++ Code/Tests/trackDelayNetwork/matlab.m
@@ -25,21 +25,26 @@ a = mean(diff(sysTime))/1000
 disp('In Hz')
 1/(a/1000)
 
-%%
-%Compare actual/target velcity and actual/taget acc.
+%% Compare actual/target velcity and actual/taget acc.
+% GENERATES: step01.eps
 
 f = figure()
 set(f,'name','Velocity','numbertitle','off')
 hold all
-title('Velocity')
-plot(qdTarget(:,1),'x')
-plot(qdActual(:,1),'o')
-plot(test01)
-legend('Target vel','Actual vel')
-%xlim([248 266]);
-%ylim([-0.03 0.07])
+title('\bf Velocity')
+plot(qdTarget(:,1),'*b','MarkerSize',10)
+plot(qdActual(:,1),'or','MarkerSize',10)
+stairs(test01,'g-.','LineWidth',2)
+legend('Target velocity','Actual velocity','Step')
+xlim([248 255]);
+ylim([-0.01 0.15])
+xlabel('Sample no.');
+ylabel('Velocity (rad/s)');
+set(gcf,'paperunits','centimeters','Paperposition',[0 0 14 8])
+saveas(gcf,'../../step01.eps','psc2')
 hold off
-% 
+
+
 % f = figure()
 % hold all
 % set(f,'name','Acc','numbertitle','off')
@@ -55,8 +60,12 @@ hold off
 %%
 %Comparing velocity and acc
 
-figure()
-plot(qActual(:,1))
+f = figure()
+set(f,'name','Position','numbertitle','off')
+hold all
+stem(qTarget(:,1))
+plot(test01*1.09)
+hold off
 
 
 %% Testing our model of the robot
@@ -70,18 +79,44 @@ offset = 1.091; %Offset in actual position
 
 f = figure()
 set(f,'name','Position','numbertitle','off')
+subplot(1,2,1);
 hold all
-title('Target Position vs Model')
-plot(qTarget(:,1),'*')
+title('Target Position vs Model position')
+plot(qTarget(:,1)-offset,'*b','MarkerSize',5)
 
 %opt = stepDataOptions('InputOffset',1000);
-s = step(dPlantDelay,3)
+s = step(dPlantDelay,3);
 svector = [zeros(1,250) s'];
 svector = svector*0.4;
-plot(svector + offset,'rx')
+plot(svector ,'rx')
 
-plot(test01*2)
-legend('Target Pos','Model pos')
-%xlim([248 266]);
-%ylim([-0.03 0.07])
+%plot(test01)
+legend('Target position','Model position')
+xlim([248 550]);
+ylim([-0.03 1])
+xlabel('Sample no.');
+ylabel('Relative position [rad]')
 hold off
+
+
+subplot(1,2,2);
+hold all
+title('Close up')
+plot(qTarget(:,1)-offset,'*')
+
+%opt = stepDataOptions('InputOffset',1000);
+s = step(dPlantDelay,3);
+svector = [zeros(1,250) s'];
+svector = svector*0.4;
+plot(svector ,'rx')
+
+stairs(test01*0.02,'g-.','LineWidth',2)
+legend('Target position','Model position','Step')
+xlim([249 265]);
+%ylim([-0.03 1])
+xlabel('Sample no.');
+ylabel('Relative position [rad]')
+hold off
+
+set(gcf,'paperunits','centimeters','Paperposition',[0 0 16 10])
+saveas(gcf,'../../model01compare.eps','psc2')
